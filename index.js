602c4f092b45da1eaf7e6cdf28c57852df06727b
GrubyProgramista321 <togrodnik2006@gmail.com>
2022-06-23 00:07:14 +0200

    some changes NOT WORKING!!!

--- index.js
+++ index.js
@@ -1,233 +1,215 @@
 const pattern = [
-  "A",
-  "a",
-  "Ą",
-  "ą",
-  "B",
-  "b",
-  "C",
-  "c",
-  "Ć",
-  "ć",
-  "D",
-  "d",
-  "E",
-  "e",
-  "Ę",
-  "ę",
-  "F",
-  "f",
-  "G",
-  "g",
-  "H",
-  "h",
-  "I",
-  "i",
-  "J",
-  "j",
-  "K",
-  "k",
-  "L",
-  "l",
-  "Ł",
-  "ł",
-  "M",
-  "m",
-  "N",
-  "n",
-  "Ń",
-  "ń",
-  "O",
-  "o",
-  "Ó",
-  "ó",
-  "P",
-  "p",
-  "R",
-  "r",
-  "S",
-  "s",
-  "Ś",
-  "ś",
-  "T",
-  "t",
-  "U",
-  "u",
-  "W",
-  "w",
-  "Y",
-  "y",
-  "Z",
-  "z",
-  "Ź",
-  "ź",
-  "Ż",
-  "ż",
-  "q",
-  "Q",
-  "X",
-  "x",
-  "!",
-  ",",
-  ".",
-  "[",
-  "]",
-  "|",
+    "A",
+    "a",
+    "Ą",
+    "ą",
+    "B",
+    "b",
+    "C",
+    "c",
+    "Ć",
+    "ć",
+    "D",
+    "d",
+    "E",
+    "e",
+    "Ę",
+    "ę",
+    "F",
+    "f",
+    "G",
+    "g",
+    "H",
+    "h",
+    "I",
+    "i",
+    "J",
+    "j",
+    "K",
+    "k",
+    "L",
+    "l",
+    "Ł",
+    "ł",
+    "M",
+    "m",
+    "N",
+    "n",
+    "Ń",
+    "ń",
+    "O",
+    "o",
+    "Ó",
+    "ó",
+    "P",
+    "p",
+    "R",
+    "r",
+    "S",
+    "s",
+    "Ś",
+    "ś",
+    "T",
+    "t",
+    "U",
+    "u",
+    "W",
+    "w",
+    "Y",
+    "y",
+    "Z",
+    "z",
+    "Ź",
+    "ź",
+    "Ż",
+    "ż",
+    "q",
+    "Q",
+    "X",
+    "x",
+    "!",
+    ",",
+    ".",
+    "[",
+    "]",
+    "|",
 ];
-var listOfValue = [];
-var listOfLetters = [];
-var text = "ale to jest fajne";
-var letters = text.split(" ");
-var textDouble = text;
-console.log(letters)
-var jasienieznam = 1;
-var newText = "";
-var nawet = 0;
+var listOfLetters = []; // nasze literki / wartosci z literek
+var text = "ale niewiem tak niepowiem";
+var letters = text.split(" "); // lista z poszczegółnymi słowami
+var textDouble = text; // duplikat textu
+var newText = ""; // text gdzie dodajemy nasze słowa
+var indexOfLetters = 0; // index naszego textu
 var shiftClcked = true;
-var currnetWordTak;
+var currnetWord = letters[indexOfLetters]
+var isTheSame = false;
+var theSameLength = false;
 for (var i = 0; i < letters.length; i++) {
-  newText += `<span class="wordsiema" id="word">${letters[i]}</span>`;
+    newText += `<span class="wordsiema" id="word">${letters[i]}</span>`;
 }
 $(document).ready(function () {
-    function currnetWord(wordNumber) {
-        currnetWordTak = letters[wordNumber]
-        console.log("currnetWordTak: ", currnetWordTak)
-        return currnetWordTak
-    }
-  $("#word-inner").html(newText);
-  for (var x = 0; x < $(".wordsiema").length; x++) {
-    var siemaaaa = "";
-    for (let i = 0; i < $(".wordsiema")[x].innerHTML.length; i++) {
-      siemaaaa += `<letter>${$(".wordsiema")[x].innerHTML[i]}</letter>`;
-    }
-    $(".wordsiema")[x].innerHTML = siemaaaa;
-  }
-  $(".keys").click(function () {
-    $("#text-keyboard").focus();
-    $("letter").css("color", "black");
-    for (var x = 0; x < $("letter").length; x++) {
-      $("letter")[x].style.color = "black";
-    }
-    $(this).css("background-color", "green");
-    if (pattern.includes($(this).val())) {
-      $("#text-keyboard")[0].value += $(this).val();
-    } else {
-      if ($(this).val() == "Backspace") {
-        var lastletter = $("#text-keyboard")[0].value.substring(
-          $("#text-keyboard")[0].value.length - 1
-        );
-        var costam = $("#text-keyboard")[0].value.replace(lastletter, "");
-        $("#text-keyboard")[0].value = costam;
-      }
-      if ($(this).val() == "shift") {
-        for (var i = 0; i < $(".keys").length; i++) {
-          if (pattern.includes($(".keys")[i].value)) {
-            $(".keys")[i].value = $(".keys")[i].value.toUpperCase();
-            $(".keys")[i].innerHTML = $(".keys")[i].value.toUpperCase();
-          }
+    $("#word-inner").html(newText);
+    for (var x = 0; x < $(".wordsiema").length; x++) {
+        var siemaaaa = "";
+        for (let i = 0; i < $(".wordsiema")[x].innerHTML.length; i++) {
+            siemaaaa += `<letter>${$(".wordsiema")[x].innerHTML[i]}</letter>`;
         }
+        $(".wordsiema")[x].innerHTML = siemaaaa;
+    }
+    $(".keys").click(function () {
+        $("#text-keyboard").focus();
+        $("letter").css("color", "black");
+        for (var x = 0; x < $("letter").length; x++) {
+            $("letter")[x].style.color = "black";
+        }
+        $(this).css("background-color", "green");
         if (pattern.includes($(this).val())) {
-          shiftClcked = false;
+            $("#text-keyboard")[0].value += $(this).val();
         } else {
-          if (!shiftClcked) {
-            shiftClcked = true;
-          } else {
-            shiftClcked = false;
-          }
-        }
-      }
-      if (shiftClcked) {
-        for (var i = 0; i < $(".keys").length; i++) {
-          if (pattern.includes($(".keys")[i].value)) {
-            console.log($(".keys")[i].value.toLowerCase());
-            $(".keys")[i].value = $(".keys")[i].value.toLowerCase();
-            $(".keys")[i].innerHTML = $(".keys")[i].value.toLowerCase();
-          }
-        }
-      }
-    }
-  });
-  for (let i = 0; i < $(".keys").length; i++) {
-    listOfLetters.push($(".keys")[i]);
-    listOfValue.push($(".keys")[i].value);
-  }
-  $(document).on("keydown", function (e) {
-    for (var i = 0; i < $(".letter").length; i++) {
-      $(".letter")[i].style.color = "black";
-    }
-    if (e.key == "Enter") {
-      e.preventDefault();
-    }
-    for (var i = 0; i < listOfLetters.length; i++) {
-      if (
-        listOfLetters[i].value == e.key ||
-        listOfLetters[i].value.toUpperCase() == e.key.toUpperCase()
-      ) {
-        listOfLetters[i].style.backgroundColor = "green";
-      }
-    }
-  });
-  $(document).on("keyup", function (e) {
-    for (var i = 0; i < listOfLetters.length; i++) {
-      if (
-        listOfLetters[i].value == e.key ||
-        listOfLetters[i].value.toUpperCase() == e.key.toUpperCase()
-      ) {
-        listOfLetters[i].style.backgroundColor = "white";
-      }
-    }
-  });
-  function changeWordValue() {
-    nawet++
-    console.log("nawet: ", nawet)
-    currnetWord(nawet)
-  }
-  function checkWord(valueUser, word) {
-    for (var i = 0; i < $("#text-keyboard").val().length; i++) {
-      if (typeof valueUser[i] == "undefined") {
-        break;
-      } else {
-        console.log(word)
-        if (valueUser[i] == word[i]) {
-          word = word.replace(word[i], "!");
-        } else if (valueUser[x] != word[x]) {
-          word = word.replace(word[i], "@");
-        }
-      }
-      console.log(word)
-      }
-      for (var x = 0; x < $("#text-keyboard").val().length; x++) {
-        if (word[x] == "!") {
-          $("letter")[x].style.color = "green";
-        } else {
-          try {
-            $("letter")[x].style.color = "red";
-          } catch (error) {}
-        }
-      }
-      console.log(word.length)
-      console.log(valueUser.length)
-      if (word.length == valueUser.length) {
-        document.getElementById("text-keyboard").addEventListener("keydown", function(e) {
-            if (e.code == "Space") {
-                e.preventDefault()
-                let lengthPoints = 0
-                for(var p = 0; p < word.length; p++) {
-                    if (word[p] == "!") {
-                        lengthPoints++
-                        console.log(lengthPoints)
+            if ($(this).val() == "Backspace") {
+                var lastletter = $("#text-keyboard")[0].value.substring(
+                    $("#text-keyboard")[0].value.length - 1
+                );
+                var costam = $("#text-keyboard")[0].value.replace(lastletter, "");
+                $("#text-keyboard")[0].value = costam;
+            }
+            if ($(this).val() == "shift") {
+                for (var i = 0; i < $(".keys").length; i++) {
+                    if (pattern.includes($(".keys")[i].value)) {
+                        $(".keys")[i].value = $(".keys")[i].value.toUpperCase();
+                        $(".keys")[i].innerHTML = $(".keys")[i].value.toUpperCase();
                     }
                 }
-                if (lengthPoints == word.length) {
-                  console.log("wszystko dobrze wariacie")
-                  changeWordValue()
-                  document.getElementById("text-keyboard").value = ""
+                if (pattern.includes($(this).val())) {
+                    shiftClcked = false;
+                } else {
+                    if (!shiftClcked) {
+                        shiftClcked = true;
+                    } else {
+                        shiftClcked = false;
+                    }
                 }
             }
-        })
+            if (shiftClcked) {
+                for (var i = 0; i < $(".keys").length; i++) {
+                    if (pattern.includes($(".keys")[i].value)) {
+                        console.log($(".keys")[i].value.toLowerCase());
+                        $(".keys")[i].value = $(".keys")[i].value.toLowerCase();
+                        $(".keys")[i].innerHTML = $(".keys")[i].value.toLowerCase();
+                    }
+                }
+            }
+        }
+    });
+    for (let i = 0; i < $(".keys").length; i++) {
+        listOfLetters.push($(".keys")[i]);
     }
-  }
-  $("#text-keyboard").on("input", function () {
-    checkWord($("#text-keyboard").val(),currnetWord(nawet));
-  });
+    $(document).on("keydown", function (e) { // press key 
+        for (var i = 0; i < $(".letter").length; i++) {
+            $(".letter")[i].style.color = "black";
+        }
+        if (e.key == "Enter") {
+            e.preventDefault();
+        }
+        for (var i = 0; i < listOfLetters.length; i++) {
+            if (
+                listOfLetters[i].value == e.key ||
+                listOfLetters[i].value.toUpperCase() == e.key.toUpperCase()
+            ) {
+                listOfLetters[i].style.backgroundColor = "green";
+            }
+        }
+    });
+    $(document).on("keyup", function (e) {
+        for (var i = 0; i < listOfLetters.length; i++) {
+            if (
+                listOfLetters[i].value == e.key ||
+                listOfLetters[i].value.toUpperCase() == e.key.toUpperCase()
+            ) {
+                listOfLetters[i].style.backgroundColor = "white";
+            }
+        }
+    });
+    function nextWord() {
+        indexOfLetters++;
+        currnetWord = letters[indexOfLetters]
+        console.log("to: ", currnetWord)
+        document.getElementById("text-keyboard").value = ""
+    }
+    function checkWord(valueUser, word) {
+        let goodLetters = 0;
+        for (let i = 0; i < valueUser.length; i++) {
+            if (valueUser[i] === word[i]) {
+                goodLetters++
+            }
+        }
+        console.log("goodLetters: ", goodLetters);
+        console.log("word.length: ", word.length)
+        // console.log("valueUser.length: ", valueUser.length)
+        if (valueUser.length == word.length) {
+            console.log("jest tyle samo")
+            theSameLength = true
+        }
+        else {
+            theSameLength = false
+        }
+        if (goodLetters === valueUser.length) {
+            isTheSame = true
+        }
+        else {
+            isTheSame = false
+        }
+    }
+    $("#text-keyboard").on("input", function () {
+        checkWord($("#text-keyboard").val(), currnetWord);
+    });
+    $("#text-keyboard").on("keydown", function (e) {
+        if (e.code === "Space") {
+            console.log("isTheSame: ", isTheSame)
+            console.log("theSameLength: ", theSameLength)
+            if (isTheSame && theSameLength) {
+                nextWord()
+            }
+        }
+    })
 });
