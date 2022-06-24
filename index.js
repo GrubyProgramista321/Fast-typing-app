90f06c1974e2a9af4711e1ae10237b65d92d0a50
GrubyProgramista321 <togrodnik2006@gmail.com>
2022-06-24 13:54:27 +0200

    big update! NOT WORKING!!!

--- index.js
+++ index.js
@@ -75,34 +75,34 @@ const pattern = [
     "|",
 ];
 var listOfLetters = []; // nasze literki / wartosci z literek
-var text = "ale niewiem tak niepowiem";
+var text = "szedl sobie ziomek co mial bardzo duzo poziomek ale niestety spadl i sie zapadl w depresje ";
 var letters = text.split(" "); // lista z poszczegółnymi słowami
 var textDouble = text; // duplikat textu
 var newText = ""; // text gdzie dodajemy nasze słowa
-var indexOfLetters = 0; // index naszego textu
-var shiftClcked = true;
-var currnetWord = letters[indexOfLetters]
-var isTheSame = false;
-var theSameLength = false;
+var indexOfWord = 0; // index naszego textu
+var shiftClcked = true; // shift klikniety na klaiwatórze ekranowej (umieszczonej na stronie)
+var currnetWord = letters[indexOfWord] // słowo które weryfikuje program
+var currnetWordColor = currnetWord; // słowo które zmienia swoją wartosc na ! i @ aby wprowadzic kolory
+var SpaceClicked = false // czy spacja kliknieta
 for (var i = 0; i < letters.length; i++) {
-    newText += `<span class="wordsiema" id="word">${letters[i]}</span>`;
+    newText += `<span class="word" id="word">${letters[i]}</span>`;
 }
 $(document).ready(function () {
     $("#word-inner").html(newText);
-    for (var x = 0; x < $(".wordsiema").length; x++) {
+    for (var x = 0; x < $(".word").length; x++) {
         var siemaaaa = "";
-        for (let i = 0; i < $(".wordsiema")[x].innerHTML.length; i++) {
-            siemaaaa += `<letter>${$(".wordsiema")[x].innerHTML[i]}</letter>`;
+        for (let i = 0; i < $(".word")[x].innerHTML.length; i++) {
+            siemaaaa += `<letter class="letters">${$(".word")[x].innerHTML[i]}</letter>`;
         }
-        $(".wordsiema")[x].innerHTML = siemaaaa;
+        $(".word")[x].innerHTML = siemaaaa;
     }
     $(".keys").click(function () {
+
         $("#text-keyboard").focus();
         $("letter").css("color", "black");
         for (var x = 0; x < $("letter").length; x++) {
-            $("letter")[x].style.color = "black";
+            $("letter")[x].style.color = "white";
         }
-        $(this).css("background-color", "green");
         if (pattern.includes($(this).val())) {
             $("#text-keyboard")[0].value += $(this).val();
         } else {
@@ -133,13 +133,13 @@ $(document).ready(function () {
             if (shiftClcked) {
                 for (var i = 0; i < $(".keys").length; i++) {
                     if (pattern.includes($(".keys")[i].value)) {
-                        console.log($(".keys")[i].value.toLowerCase());
                         $(".keys")[i].value = $(".keys")[i].value.toLowerCase();
                         $(".keys")[i].innerHTML = $(".keys")[i].value.toLowerCase();
                     }
                 }
             }
         }
+        checkWord($("#text-keyboard").val(), currnetWord2o());
     });
     for (let i = 0; i < $(".keys").length; i++) {
         listOfLetters.push($(".keys")[i]);
@@ -170,45 +170,127 @@ $(document).ready(function () {
             }
         }
     });
-    function nextWord() {
-        indexOfLetters++;
-        currnetWord = letters[indexOfLetters]
-        console.log("to: ", currnetWord)
-        document.getElementById("text-keyboard").value = ""
-    }
-    function checkWord(valueUser, word) {
-        let goodLetters = 0;
-        for (let i = 0; i < valueUser.length; i++) {
-            if (valueUser[i] === word[i]) {
-                goodLetters++
+
+    function addLettersColored(userInput) {
+        let fajnie = userInput.substring(currnetWord2o().length)
+        let additionalLetters = ""
+        additionalLetters += "<div class='costam'>"
+        for (let i = 0; i <= fajnie.length; i++) {
+            if (typeof (fajnie[i]) == "undefined") {
+                break;
+            }
+            let lettersAppend = `<letters class="additional-letters">${fajnie[i]}</letters>`
+            additionalLetters += lettersAppend
+        }
+        additionalLetters += "</div>"
+        console.log(additionalLetters)
+        $(".word")[indexOfWord].innerHTML += additionalLetters
+        // console.log(additionalLetters)
+        let listOfAdditionalLetters = []
+        let parent = $(".word")[indexOfWord].children
+        for (let y = 0; y < parent.length; y++) {
+            console.log(parent[y].className)
+            if (parent[y].className == "additional-letters") {
+                listOfAdditionalLetters.push(parent[y])
             }
         }
-        console.log("goodLetters: ", goodLetters);
-        console.log("word.length: ", word.length)
-        // console.log("valueUser.length: ", valueUser.length)
-        if (valueUser.length == word.length) {
-            console.log("jest tyle samo")
-            theSameLength = true
-        }
-        else {
-            theSameLength = false
-        }
-        if (goodLetters === valueUser.length) {
-            isTheSame = true
-        }
-        else {
-            isTheSame = false
+        console.log(listOfAdditionalLetters)
+    }
+    function nextWord() {
+        let superancko = $("#text-keyboard").val()
+        $(".word")[indexOfWord].ariaValue = `${superancko}`
+        indexOfWord++
+    }
+    function PrviousWord() {
+        console.log($(".word")[indexOfWord].previousElementSibling)
+        if ($(".word")[indexOfWord].previousElementSibling != null) {
+            console.log($(".word")[indexOfWord].previousElementSibling.ariaValue)
+            document.getElementById("text-keyboard").value = $(".word")[indexOfWord].previousElementSibling.ariaValue.split(" ")[0]
+            indexOfWord--
         }
     }
+    function currnetWord2o() {
+        currnetWord = letters[indexOfWord]
+        return currnetWord
+    }
+    function checkWordValid(wordWithColors) {
+        var validPoints = 0;
+        for (let i = 0; i < wordWithColors.length; i++) {
+            if (currnetWordColor[i] === "!") {
+                validPoints++
+            }
+        }
+        console.log("validPoints: ", validPoints)
+        console.log("word.length: ", wordWithColors.length)
+        console.log("userInput.length", document.getElementById("text-keyboard").value.length)
+        if (validPoints === wordWithColors.length && validPoints === document.getElementById("text-keyboard").value.length) {
+            $(".word")[indexOfWord].ariaLabel = "correct"
+            console.log($(".word")[indexOfWord].ariaLabel)
+        }
+        else {
+            $(".word")[indexOfWord].ariaLabel = ""
+        }
+    }
+
+    function changeColor(thisWord) {
+        for (let x = 0; x < thisWord.length; x++) {
+            $(".word")[indexOfWord].children[x].style.color = "white"
+        }
+        for (let i = 0; i < thisWord.length; i++) {
+            let colorLetter = $(".word")[indexOfWord].children[i].style.color;
+            if (currnetWordColor[i] == "!") {
+                $(".word")[indexOfWord].children[i].style.color = "green"
+            }
+            else if (currnetWordColor[i] == "@") {
+                $(".word")[indexOfWord].children[i].style.color = "red"
+            }
+        }
+    }
+
+    function checkWord(valueUser, word) {
+        let goodLetters = 0;
+
+        console.log("tak", childrenElements)
+        var parent = $(".word")[indexOfWord]
+        var childrenElements = parent.querySelectorAll(".costam")
+        try {
+            childrenElements.children = ''
+        }
+        catch (error) {
+        }
+        currnetWordColor = currnetWord
+        for (let i = 0; i < word.length; i++) {
+            if (valueUser[i] == currnetWord[i]) {
+                currnetWordColor = currnetWordColor.replace(currnetWord[i], "!")
+            }
+            else {
+                if (typeof (valueUser[i]) != "undefined") {
+                    currnetWordColor = currnetWordColor.replace(currnetWord[i], "@")
+                }
+            }
+        }
+        if (valueUser.length > word.length) {
+            addLettersColored(valueUser)
+        }
+        changeColor(currnetWordColor)
+        checkWordValid(currnetWordColor)
+        console.log(childrenElements)
+    }
+
     $("#text-keyboard").on("input", function () {
-        checkWord($("#text-keyboard").val(), currnetWord);
+        checkWord($("#text-keyboard").val(), currnetWord2o());
     });
     $("#text-keyboard").on("keydown", function (e) {
         if (e.code === "Space") {
-            console.log("isTheSame: ", isTheSame)
-            console.log("theSameLength: ", theSameLength)
-            if (isTheSame && theSameLength) {
-                nextWord()
+            nextWord()
+            checkWordValid(currnetWordColor)
+            document.getElementById("text-keyboard").value = ""
+            e.preventDefault()
+        }
+        if (e.code == "Backspace") {
+            if ($("#text-keyboard").val() <= 0) {
+                e.preventDefault()
+                PrviousWord()
             }
         }
     })
